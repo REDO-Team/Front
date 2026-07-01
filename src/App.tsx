3be1443d988c05412b3fb62c2a9946e49be6e8f1
somin2352 <164002759+somin2352@users.noreply.github.com>
2026-07-02 02:31:21 +0900

    [Design] 글자 색상 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1 className='h-10 bg-main-gradient text-gray-100 font-pretendard font-bold text-2xl'>REDO</h1>
+      <h1 className='bg-main-gradient text-text font-pretendard font-bold text-2xl'>REDO</h1>
     </>
   );
 }
