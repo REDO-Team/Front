8fb3cc62ada3e931bb42aeb06745fda6d8bc75d9
somin2352 <164002759+somin2352@users.noreply.github.com>
2026-07-09 20:12:20 +0900

    [Build] vite-plugin-svgr 라이브러리 설치

---
+++ src/types/svg.d.ts
@@ -0,0 +1,6 @@
+// src/types/svg.d.ts (또는 src/types.d.ts)
+declare module '*.svg?react' {
+  import { FC, SVGProps } from 'react';
+  const ReactComponent: FC<SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
