5b5666ffb492c7fb58d61b2f38e8b2808f8e5a13
somin2352 <164002759+somin2352@users.noreply.github.com>
2026-07-10 20:17:16 +0900

    [Design] 레이아웃 수정
    - 페이지 배경색 조건 처리
    - 상단바 고정에 따른 높이 계산
    - 인증 가이드 페이지 상단바 조건 추가
    - 레이아웃 수정에 따른 인증페이지 여백 및 높이 수정

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -10,18 +10,22 @@ const Layout = () => {
   const isSplash = !!matchPath('/splash', location.pathname);
   const isGuide = !!matchPath('/guide', location.pathname);
   const isCertification = !!matchPath('/certification', location.pathname);
+  const isCertificationGuide = !!matchPath('/certification/guide', location.pathname);
 
   const hideTopBar = isSplash;
-  const hideBottomBar = isSplash || isCertification;
+  const hideBottomBar = isSplash || isCertification || isCertificationGuide;
 
   return (
-    <div className='min-h-screen bg-white'>
+    <div className={`min-h-screen ${isGuide || isCertification || isCertificationGuide ? 'bg-bg-green1' : 'bg-white'}`}>
       <div className='mx-auto min-h-screen w-full max-w-120'>
-        {!hideTopBar && (isGuide ? <TopBar title='이용 가이드' leftIcon /> : isCertification ? <TopBar title='인증하기' leftIcon rightIcon={Home} onClick={() => navigate('/')} /> : <TopBar />)}
-        <main className='flex-1'>
-          <Outlet />
-        </main>
-        {!hideBottomBar && <BottomBar />}
+        {!hideTopBar && (isGuide ? <TopBar title='이용 가이드' leftIcon /> : isCertification ? <TopBar title='인증하기' leftIcon rightIcon={Home} onClick={() => navigate('/')} /> : isCertificationGuide ? <TopBar title='인증 가이드' leftIcon rightIcon={Home} onClick={() => navigate('/')} /> : <TopBar />)}
+        <div className={`mx-auto flex w-full max-w-120 flex-col ${hideTopBar ? 'min-h-dvh' : 'h-[calc(100dvh-56px)] pt-14'}`}>
+          <main className='flex-1'>
+            <Outlet />
+          </main>
+
+          {!hideBottomBar && <BottomBar />}
+        </div>
       </div>
     </div>
   );
