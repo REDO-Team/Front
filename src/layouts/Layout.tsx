fecefedd6361059b8d93bf4069ad4bd94398a544
somin2352 <164002759+somin2352@users.noreply.github.com>
2026-07-10 18:02:35 +0900

    [Modify] 상단바 조건 처리 수정
    - 인증, 가이드 페이지일 경우 조건 처리 코드 변경

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,23 +1,28 @@
-import { matchPath, Outlet, useLocation } from 'react-router-dom';
+import { matchPath, Outlet, useLocation, useNavigate } from 'react-router-dom';
 import BottomBar from '../components/common/BottomBar';
 import TopBar from '../components/common/TopBar';
+import Home from '/src/assets/icons/home.svg';
 
 const Layout = () => {
   const location = useLocation();
+  const navigate = useNavigate();
 
   const isSplash = !!matchPath('/splash', location.pathname);
   const isGuide = !!matchPath('/guide', location.pathname);
+  const isCertification = !!matchPath('/certification', location.pathname);
 
   const hideTopBar = isSplash;
-  const hideBottomBar = isSplash;
+  const hideBottomBar = isSplash || isCertification;
 
   return (
     <div className='min-h-screen bg-white'>
-      <main className='mx-auto min-h-screen w-full max-w-120'>
-        {!hideTopBar && isGuide ? <TopBar title='이용 가이드' leftIcon /> : <TopBar />}
-        <Outlet />
+      <div className='mx-auto min-h-screen w-full max-w-120'>
+        {!hideTopBar && (isGuide ? <TopBar title='이용 가이드' leftIcon /> : isCertification ? <TopBar title='인증하기' leftIcon rightIcon={Home} onClick={() => navigate('/')} /> : <TopBar />)}
+        <main className='flex-1'>
+          <Outlet />
+        </main>
         {!hideBottomBar && <BottomBar />}
-      </main>
+      </div>
     </div>
   );
 };
