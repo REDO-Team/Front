18ba846708a3ff88ffc0b13594b418382be475fe
somin2352 <164002759+somin2352@users.noreply.github.com>
2026-07-10 18:01:43 +0900

    [Feat] 상단바 페이지 조건 추가 및 우측 아이콘 수정
    - 우측 아이콘 홈버튼일 경우 기능 처리
    - 인증 페이지일 경우 배경색 변경

--- src/components/common/TopBar.tsx
+++ src/components/common/TopBar.tsx
@@ -1,4 +1,3 @@
-import type { ReactNode } from 'react';
 import LeftArrow from '/src/assets/icons/left-arrow.svg';
 import { matchPath, useLocation, useNavigate } from 'react-router-dom';
 
@@ -6,17 +5,19 @@ interface TopBarProps {
   title?: string;
   leftIcon?: boolean;
   leftText?: string; // leftIcon 옆에 글자를 바로 적고 싶을 때 사용. leftIcon에서 두 덩어리로 작성해도 됨.
-  rightIcon?: ReactNode;
+  rightIcon?: string;
+  onClick?: () => void;
 }
 
-export default function TopBar({ title, leftIcon, leftText, rightIcon }: TopBarProps) {
+export default function TopBar({ title, leftIcon, leftText, rightIcon, onClick }: TopBarProps) {
   const navigate = useNavigate();
   const location = useLocation();
 
   const isGuide = matchPath('/guide', location.pathname);
+  const isCertification = !!matchPath('/certification', location.pathname);
 
   return (
-    <header className={`relative flex items-center justify-between w-full h-14 px-4 z-50 ${isGuide ? 'bg-bg-green1' : 'bg-white'}`}>
+    <header className={`relative flex items-center justify-between w-full h-14 px-4 z-50 ${isGuide || isCertification ? 'bg-bg-green1' : 'bg-white'}`}>
       {/* 왼쪽 아이콘 영역 */}
       {/* 꺾새 옆에 바로 글자를 적을 때 leftIcon에서 두 덩어리로 작성해주세요! */}
       {(leftIcon || leftText) && (
@@ -36,7 +37,11 @@ export default function TopBar({ title, leftIcon, leftText, rightIcon }: TopBarP
       )}
 
       {/* 오른쪽 아이콘 영역 */}
-      {rightIcon && <div className='ml-auto flex items-center justify-end z-10 text-base font-semibold text-text'>{rightIcon}</div>}
+      {rightIcon && (
+        <button type='button' onClick={onClick}>
+          <img src={rightIcon} alt='홈으로 가기' className='ml-auto flex items-center justify-end z-10 text-base font-semibold text-text' />
+        </button>
+      )}
     </header>
   );
 }
