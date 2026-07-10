b1922212d04486f384bbb5047203b28471e0864a
somin2352 <164002759+somin2352@users.noreply.github.com>
2026-07-10 18:03:35 +0900

    [Feat] 인증 페이지 구현
    - 필요한 아이콘 추가
    - 버튼 클릭 시 페이지 이동

---
+++ src/pages/certification-page/index.tsx
@@ -0,0 +1,60 @@
+import GradientLogo from '/src/assets/icons/gradient-logo.svg';
+import Scan from '/src/assets/icons/scan.svg';
+import Seed from '/src/assets/icons/seed.svg';
+import Seedling from '/src/assets/icons/seedling.svg';
+import RightArrow from '/src/assets/icons/right-arrow.svg?react';
+import { useNavigate } from 'react-router-dom';
+
+export default function CertificationPage() {
+  const navigate = useNavigate();
+
+  return (
+    <div className='flex flex-col items-center pt-10.5 px-6.5 bg-bg-green1 min-h-screen'>
+      <div className='relative w-47.5 h-47.5'>
+        <img src={Scan} alt='스캔' className='absolute inset-0 w-full h-full' />
+        <img src={GradientLogo} alt='로고' className='absolute w-23 h-23 top-1/2 left-1/2 -translate-1/2' />
+      </div>
+
+      <div className='mt-2.5'>
+        <p className='font-bold text-[22px] text-center text-main-green1'>
+          <span className='text-text'>작은 인증이</span>
+          <br />
+          지구를 지키는 큰 변화
+        </p>
+        <p className='text-base text-center text-gray-500 font-semibold'>오늘도 ReDO!와 함께해요</p>
+      </div>
+
+      <div className='w-full px-6.5 py-4.5 bg-white mt-15 flex flex-col gap-4'>
+        <div className='flex justify-between items-center w-full'>
+          <div className='flex items-center gap-4'>
+            <img src={Seed} alt='인증' className='w-12.5 h-12.5' />
+            <span className='font-pretendard font-semibold text-base text-text'>1일 최대 인증 횟수</span>
+          </div>
+          <span className='font-pretendard font-bold text-[22px] text-main-green1'>
+            3<span className='text-gray-600'>회</span>
+          </span>
+        </div>
+        <div className='flex justify-between items-center w-full'>
+          <div className='flex items-center gap-4'>
+            <img src={Seedling} alt='인증' className='w-12.5 h-12.5' />
+            <span className='font-pretendard font-semibold text-base text-text'>잔여 인증 횟수</span>
+          </div>
+          <span className='font-pretendard font-bold text-[22px] text-main-green1'>
+            1<span className='text-gray-600'>/3회</span>
+          </span>
+        </div>
+      </div>
+
+      <div className='flex flex-col w-full gap-2.5 mt-18 mb-5'>
+        <button type='button' className='font-pretendard font-bold text-xl text-white rounded-4xl bg-main-green1 px-5 py-4 w-full flex items-center justify-center relative' onClick={() => navigate('/certification/photo')}>
+          <span>인증하기</span>
+          <RightArrow className='absolute right-5' />
+        </button>
+        <button type='button' className='font-pretendard font-bold text-xl text-main-green1 rounded-4xl bg-white border border-main-green1 px-5 py-4 w-full flex items-center justify-center relative' onClick={() => navigate('/certification/guide')}>
+          <span>인증 가이드 보기</span>
+          <RightArrow className='absolute right-5' />
+        </button>
+      </div>
+    </div>
+  );
+}
