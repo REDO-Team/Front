2dd45cfd70434138f02d89336672bcc47246bce3
somin2352 <164002759+somin2352@users.noreply.github.com>
2026-07-12 00:51:00 +0900

    [Refactor] 재활용품 배출 정보 카드 재사용을 위한 컴포넌트 분리

--- src/components/GuidePage/GuideCard.tsx
+++ src/components/GuidePage/GuideCard.tsx
@@ -4,6 +4,7 @@ import Check from '/src/assets/icons/check.svg?react';
 import FullCheck from '/src/assets/icons/full-check.svg';
 import ImageUpload from '/src/assets/icons/image-upload.svg';
 import TextSearch from '/src/assets/icons/text-search.svg';
+import RecycleCategoryCard from '../common/RecycleCategoryCard';
 
 interface GuideCardProps {
   Icon: React.FC<React.SVGProps<SVGSVGElement>>;
@@ -80,13 +81,7 @@ export default function GuideCard({ Icon, stepNum, text, onClick, isOpen }: Guid
           {isOpen && stepNum === 3 && (
             <div className='flex flex-col gap-2.5'>
               <p className='pt-3.5 text-gray-800 font-pretendard font-medium text-[15px] break-keep'>AI 분석 결과로 올바른 분리배출 방법을 확인해요. 재활용 카테고리와 배출 방법, 추가 주의사항을 안내해드려요.</p>
-              <div className='flex p-2.5 gap-2 bg-linear-to-r from-main-green1 to-main-sky rounded-xl'>
-                <div className='w-10 h-10 opacity-45 bg-white rounded-[10px]'></div>
-                <div className='flex flex-col'>
-                  <span className='font-pretendard font-semibold text-[8px] text-white bg-bg-green3/50 px-2 py-0.5 rounded-full inline-fit'>플라스틱</span>
-                  <span className='font-pretendard font-bold text-base text-white'>투명 페트병</span>
-                </div>
-              </div>
+              <RecycleCategoryCard padding={10} gap={8} radius={10} imgSize={40} categorySize={8} nameSize={16} />
               <div className='flex flex-col gap-1 pt-1'>
                 <div className='flex gap-3'>
                   <img src={FullCheck} alt='확인하기' className='w-4.5 h-4.5' />
