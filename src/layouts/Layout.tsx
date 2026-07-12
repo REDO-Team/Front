72183d9ef2134f89e16f46e15f98333cfc4926b9
somin2352 <164002759+somin2352@users.noreply.github.com>
2026-07-12 20:13:25 +0900

    [Modify] 배출 정보 탐색 실패 페이지 추가 및 조건 누락 페이지 추가

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -13,96 +13,30 @@ const Layout = () => {
 
   const isGuide = !!matchPath('/guide', location.pathname);
 
-  const isCertification = !!matchPath(
-    '/certification',
-    location.pathname,
-  );
-  const isCertificationGuide = !!matchPath(
-    '/certification/guide',
-    location.pathname,
-  );
-  const isCertificationShoot = !!matchPath(
-    '/certification/shooting',
-    location.pathname,
-  );
-  const isCertificationSuccess = !!matchPath(
-    '/certification/success',
-    location.pathname,
-  );
-  const isCertificationFail = !!matchPath(
-    '/certification/fail',
-    location.pathname,
-  );
+  const isCertification = !!matchPath('/certification', location.pathname);
+  const isCertificationGuide = !!matchPath('/certification/guide', location.pathname);
+  const isCertificationShoot = !!matchPath('/certification/shooting', location.pathname);
+  const isCertificationSuccess = !!matchPath('/certification/success', location.pathname);
+  const isCertificationFail = !!matchPath('/certification/fail', location.pathname);
 
-  const isDisposalInfo = !!matchPath(
-    '/disposal-info',
-    location.pathname,
-  );
-  const isDisposalInfoDetail = !!matchPath(
-    '/disposal-info/detail',
-    location.pathname,
-  );
-  const isImageSearch = !!matchPath(
-    '/disposal-info/image-search',
-    location.pathname,
-  );
-  const isProblemSearch = !!matchPath(
-    '/disposal-info/problem-search',
-    location.pathname,
-  );
+  const isDisposalInfo = !!matchPath('/disposal-info', location.pathname);
+  const isDisposalInfoDetail = !!matchPath('/disposal-info/detail', location.pathname);
+  const isDisposalInfoFail = !!matchPath('/disposal-info/fail', location.pathname);
+  const isImageSearch = !!matchPath('/disposal-info/image-search', location.pathname);
+  const isProblemSearch = !!matchPath('/disposal-info/problem-search', location.pathname);
 
-  const isMyContribution = !!matchPath(
-    '/my-contribution',
-    location.pathname,
-  );
+  const isMyContribution = !!matchPath('/my-contribution', location.pathname);
 
-  const hideTopBar =
-    isHome ||
-    isSplash ||
-    isLogin ||
-    isSignup ||
-    isSignupTerms;
+  const hideTopBar = isHome || isSplash || isLogin || isSignup || isSignupTerms;
 
-  const hideBottomBar =
-    isSplash ||
-    isLogin ||
-    isSignup ||
-    isSignupTerms ||
-    isCertification ||
-    isCertificationGuide ||
-    isCertificationShoot ||
-    isCertificationSuccess ||
-    isCertificationFail ||
-    isImageSearch ||
-    isProblemSearch ||
-    isMyContribution;
+  const hideBottomBar = isSplash || isLogin || isSignup || isSignupTerms || isCertification || isCertificationGuide || isCertificationShoot || isCertificationSuccess || isCertificationFail || isImageSearch || isProblemSearch || isMyContribution || isDisposalInfoFail || isDisposalInfoDetail;
 
-  const hasGreenBackground =
-    isGuide ||
-    isCertification ||
-    isCertificationGuide ||
-    isCertificationShoot ||
-    isCertificationSuccess ||
-    isCertificationFail ||
-    isDisposalInfo ||
-    isDisposalInfoDetail ||
-    isImageSearch ||
-    isProblemSearch;
+  const hasGreenBackground = isGuide || isCertification || isCertificationGuide || isCertificationShoot || isCertificationSuccess || isDisposalInfo || isDisposalInfoDetail || isImageSearch || isProblemSearch || isDisposalInfoFail;
 
   return (
-    <div
-      className={`min-h-screen overflow-auto ${
-        hasGreenBackground ? 'bg-bg-green1' : 'bg-white'
-      }`}
-    >
+    <div className={`min-h-screen overflow-auto ${hasGreenBackground ? 'bg-bg-green1' : 'bg-white'}`}>
       <div className='mx-auto min-h-screen w-full max-w-120'>
-        <div
-          className={`mx-auto flex w-full max-w-120 flex-col ${
-            hideTopBar
-              ? 'min-h-dvh'
-              : 'h-[calc(100dvh-56px)] pt-14'
-          }`}
-        >
+        <div className={`mx-auto flex w-full max-w-120 flex-col ${hideTopBar ? 'min-h-dvh' : 'h-[calc(100dvh-56px)] pt-14'}`}>
           <main className='flex flex-1 flex-col'>
             <Outlet />
           </main>
