5595933f151c0f123b64554b578f8929d27d475e
somin2352 <164002759+somin2352@users.noreply.github.com>
2026-07-13 00:39:41 +0900

    [Modify] 상단바 색상 및 배치 매개변수 전달 추가

--- src/components/common/TopBar.tsx
+++ src/components/common/TopBar.tsx
@@ -1,4 +1,4 @@
-import LeftArrow from '/src/assets/icons/left-arrow.svg';
+import LeftArrow from '/src/assets/icons/left-arrow.svg?react';
 import { useNavigate } from 'react-router-dom';
 
 interface TopBarProps {
@@ -8,28 +8,36 @@ interface TopBarProps {
   rightIcon?: string;
   onClick?: () => void;
   bgColor?: string;
+  color?: string;
+  position?: string;
 }
 
-export default function TopBar({ title, leftIcon, leftText, rightIcon, onClick, bgColor = 'white' }: TopBarProps) {
+export default function TopBar({ title, leftIcon, leftText, rightIcon, onClick, bgColor = 'white', color = 'black', position = 'fixed' }: TopBarProps) {
   const navigate = useNavigate();
 
   return (
-    <header className={`fixed top-0 w-full max-w-120 flex items-center justify-between h-14 px-4 z-50 bg-${bgColor}`}>
+    <header className={`${position} top-0 w-full max-w-120 flex items-center justify-between h-14 px-4 z-50 bg-${bgColor}`}>
       {/* 왼쪽 아이콘 영역 */}
       {/* 꺾새 옆에 바로 글자를 적을 때 leftIcon에서 두 덩어리로 작성해주세요! */}
       {(leftIcon || leftText) && (
         <div className='flex items-center justify-center z-10 text-lg font-semibold leading-none text-text' onClick={() => navigate(-1)}>
           <button type='button' className='w-6 h-6 flex justify-center items-center cursor-pointer'>
-            {leftIcon && <img src={LeftArrow} alt='뒤로가기' />}
+            {leftIcon && <LeftArrow style={{ color }} />}
           </button>
-          {leftText && <span className='text-lg font-semibold leading-none text-center text-black'>{leftText}</span>}
+          {leftText && (
+            <span className='text-lg font-semibold leading-none text-center text-black' style={{ color }}>
+              {leftText}
+            </span>
+          )}
         </div>
       )}
 
       {/* 가운데 제목 영역 */}
       {title && (
         <div className='absolute inset-0 flex items-center justify-center px-12 pointer-events-none'>
-          <span className='max-w-full truncate whitespace-nowrap text-lg font-pretendard font-semibold leading-none text-center text-text'>{title}</span>
+          <span className='max-w-full truncate whitespace-nowrap text-lg font-pretendard font-semibold leading-none text-center text-text' style={{ color }}>
+            {title}
+          </span>
         </div>
       )}
 
