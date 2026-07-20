ed05d723f6f093017079fa98ec0fca2a93507e03
somin2352 <164002759+somin2352@users.noreply.github.com>
2026-07-20 18:18:34 +0900

    [Feat] 일반 쓰레기 카테고리 적용 시 분기 처리
    - 카드 디자인 변경
    - 하단 버튼 변경
    - Info 카드 유무에 따른 조건 처리

--- src/pages/disposal-info-page/detail-page.tsx
+++ src/pages/disposal-info-page/detail-page.tsx
@@ -11,7 +11,7 @@ import { useState } from 'react';
 const mock = [
   {
     category: '플라스틱',
-    name: '투명 페트병',
+    name: '일반쓰레기',
     content: '분리배출이 가능한 품목이에요',
     guide: ['비닐 라벨을 뜯어 따로 분리해요', '내용물을 비우고 물로 헹궈요', '찌그러뜨려 부피를 줄여요', '투명 페트병 전용함에 배출해요'],
     info: '라벨 뚜껑은 비닐/플라스틱으로 따로 버려요',
@@ -30,6 +30,14 @@ export default function DisposalInfoDetailage() {
     setIsOpen(true);
   };
 
+  const handleNavigate = () => {
+    if (mock[0].name.includes('쓰레기')) {
+      navigate('/');
+    } else {
+      navigate('/certification');
+    }
+  };
+
   return (
     <div className='h-full'>
       <div className='mb-6'>
@@ -42,19 +50,22 @@ export default function DisposalInfoDetailage() {
           <p className='font-pretendard font-bold text-lg text-text'>이렇게 배출해주세요</p>
           <div className='flex flex-col gap-5'>
             <div className='flex flex-col gap-5 px-6.5 py-6 rounded-xl bg-white shadow-lg shadow-black/5'>
-              {mock[0].guide.map((g, idx) => {
-                return (
-                  <div className='flex items-center gap-3' key={idx}>
-                    <FullCheck className='w-4.5 h-4.5' />
-                    <p className='font-pretendard font-medium text-base text-gray-800'>{g}</p>
-                  </div>
-                );
-              })}
+              {mock[0].guide &&
+                mock[0].guide?.map((g, idx) => {
+                  return (
+                    <div className='flex items-center gap-3' key={idx}>
+                      <FullCheck className='w-4.5 h-4.5' />
+                      <p className='font-pretendard font-medium text-base text-gray-800'>{g}</p>
+                    </div>
+                  );
+                })}
             </div>
-            <div className='flex items-center gap-2.5 px-6 py-3.5 bg-bg-green2 rounded-[20px]'>
-              <Info className='text-main-green1 w-6 h-6' />
-              <p className='font-pretendard font-medium text-sm text-main-green2'>{mock[0].info}</p>
-            </div>
+            {mock[0].info && (
+              <div className='flex items-center gap-2.5 px-6 py-3.5 bg-bg-green2 rounded-[20px]'>
+                <Info className='text-main-green1 w-6 h-6' />
+                <p className='font-pretendard font-medium text-sm text-main-green2'>{mock[0].info}</p>
+              </div>
+            )}
           </div>
         </div>
 
@@ -63,8 +74,8 @@ export default function DisposalInfoDetailage() {
             <img src={Favorites} alt='즐겨찾기' />
             <span>즐겨찾기 저장</span>
           </button>
-          <button type='button' className='font-pretendard font-bold text-lg text-white rounded-4xl bg-main-green1 px-5 py-4 w-full flex items-center justify-center' onClick={() => navigate('/certification')}>
-            가이드대로 배출 후 인증
+          <button type='button' className='font-pretendard font-bold text-lg text-white rounded-4xl bg-main-green1 px-5 py-4 w-full flex items-center justify-center' onClick={handleNavigate}>
+            {mock[0].name.includes('쓰레기') ? '홈으로' : '가이드대로 배출 후 인증'}
           </button>
         </div>
 
